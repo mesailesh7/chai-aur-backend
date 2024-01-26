2fc25492019a56374cb3350a447e77c087344eaa
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2024-01-26 00:05:48 -0700

    multer upload

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,3 +6,31 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    // upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded succesfully
+    console.log("file is uploaded in cloudinary", response.url);
+    return response;
+  } catch (error) {
+    //remove the locally saved temporary file as the upload operation got failed
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+// got it from cloudinary and implemented it above in async function
+// cloudinary.uploader.upload(
+//   "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" },
+//   function (error, result) {
+//     console.log(result);
+//   }
+// );
+
+export { uploadOnCloudinary };
