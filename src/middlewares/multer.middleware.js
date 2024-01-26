29d7e671ca9d5842eff5f80cba609125e8afb2b7
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2024-01-26 00:24:25 -0700

    Update multer.middleware.js

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,6 +1,9 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
+  // in request you get json data from the body
+  // in file: file is the file you want to upload or save
+  // cb is callback
   destination: function (req, file, cb) {
     cb(null, "/tmp/my-uploads");
   },
