388c25d5555345d6950d9750a6b857d81c0c80dd
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2024-02-08 23:10:38 -0700

    Update in logout Registry
    CHapter 2: 1:07:24 update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -166,7 +166,27 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  User.findById;
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User Logged out"));
 });
 
-export { registerUser, loginUser };
+export { registerUser, loginUser, logoutUser };
